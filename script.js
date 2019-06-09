33e51486391e7bf5add51b660b0e4660e035c676
Alexander Savchenko <sasha.savchenko@gmail.com>
2019-06-09 16:23:35 +0300

    Basic task done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('Hello world!');
+//What is an object method? 
+let testUsr;
+
+function creatNewUser () {
+    let newUser = {
+        firstName: prompt('Please enter your first name'),
+        lastName: prompt('Please enter your last name'),
+        getLogin() { 
+            return (this.firstName.trim().slice(0, 1).toLowerCase() + this.lastName.trim().toLowerCase());
+        },
+    };
+return newUser;
+}
+
+testUsr = creatNewUser();
+console.log(testUsr.getLogin());
+
